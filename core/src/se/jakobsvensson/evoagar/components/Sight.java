57f0300d08cb4dfaab4418ce36e1c49840309607
Jakob Svensson <jakob-_-92@hotmail.com>
2015-08-06 16:43:10 +0200

    Added skeleton for Sight

---
+++ core/src/se/jakobsvensson/evoagar/components/Sight.java
@@ -0,0 +1,22 @@
+package se.jakobsvensson.evoagar.components;
+
+import com.artemis.Component;
+
+public class Sight extends Component{
+	
+	//Variabel för hur långt en plupp kan se (sightRadius)
+	
+	public Sight(){//Den här metoden (constructorn) ska ta in ett värde som ska sparas i sightRadius
+		
+	}
+	
+	
+	public void setSightRadius(float radius){//Den här metoden ska ta in ett värde och spara det i sightRadius
+		
+	}
+	
+	public float getSightRadius(){//Den här metoden ska ge tillbaka sightRadius när man kallar på den
+		return 0;
+	}
+	
+}
